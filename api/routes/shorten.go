0dc72c5aa9d415af4d9373c9e8624521399ad006
paul <paulbrowne2@gmail.com>
2022-03-02 23:16:59 +0000

    Added ShortenURL function to parse the body, check if input is actual URL, enforce https, SSL

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,6 +1,10 @@
 package routes
 
-import "time"
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 type request struct {
 	URL         string        `json:"url"`
@@ -14,4 +18,28 @@ type response struct {
 	Expiry         time.Duration `json:"expiry"`
 	XRateRemaining int           `json:"rate_limit"`
 	XRateLimitRest time.Duration `json:"rate_limit_reset"`
+}
+
+func ShortenURL(c *fiber.Ctx) error {
+	body := new(request)
+
+	if err != c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "cannot parse JSON"})
+	}
+
+	//implementing rate limiter from IP
+
+	//check if input is an actual URL
+
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid URL"})
+	}
+
+	//check for domain error
+	if !helpers.RemoveDomainError(body.URL) {
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error": "You cannot access this :("})
+	}
+
+	//enforce https, SSL
+	body.URL = helpers.EnforceHTTP(body.URL)
 }
