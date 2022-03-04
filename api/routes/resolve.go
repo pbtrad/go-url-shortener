394655e4c3cee5b652fc7df1efe2f0521f4aad7b
paul <paulbrowne2@gmail.com>
2022-03-04 22:35:12 +0000

    Created custom url shortener and set expiry time

--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -2,7 +2,6 @@ package routes
 
 import (
 	"github.com/go-redis/redis/v8"
-	"github.com/gofiber/fiber"
 	"github.com/gofiber/fiber/v2"
 	"github.com/pbtrad/go-url-shortener/database"
 )
@@ -15,20 +14,20 @@ func ResolveURL(c *fiber.Ctx) error {
 	defer r.Close()
 
 	value, err := r.Get(database.Ctx, url).Result()
-	if err == redis.Nil{
+	if err == redis.Nil {
 		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-			"error":"short not found in the database"
+			"error": "short not found in the database",
 		})
 	} else if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error":"cannot connect to D8",
+			"error": "cannot connect to D8",
 		})
 	}
 
 	rInr := database.CreateClient(1)
 	defer r.Inr.Close()
 
-	_= rInr.Incr(database.Ctx, "counter")
+	_ = rInr.Incr(database.Ctx, "counter")
 
 	return c.Redirect(value, 301)
 }
--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 	"github.com/pbtrad/go-url-shortener/database"
 	"github.com/pbtrad/go-url-shortener/helpers"
 )
@@ -63,6 +64,35 @@ func ShortenURL(c *fiber.Ctx) error {
 
 	//enforce https, SSL
 	body.URL = helpers.EnforceHTTP(body.URL)
+
+	var id string
+	if body.CustomShort == "" {
+		id = uuid.New().String()[:6]
+	} else {
+		id = body.CustomShort
+	}
+
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	val, _ = r.Get(database.Ctx, id).Result()
+	if val != "" {
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+			"error": "URL custom short is already in use",
+		})
+	}
+
+	if body.Expiry == 0 {
+		body.Expiry = 24
+	}
+
+	err = r.Set(database.Ctx, id, body.URL, body.Expiry*3600*time.Second).Err()
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Unable to connect to server",
+		})
+	}
 
 	r2.Decr(database.Ctx, c.IP())
 }
