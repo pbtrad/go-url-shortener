bea7ff58d9275b0b885f65599e2dc887c0b027fe
paul <paulbrowne2@gmail.com>
2022-03-04 07:22:45 +0000

    Set rate limiter to detect IP in db and limit to 10 every 30 minutes

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,9 +1,14 @@
 package routes
 
 import (
+	"os"
+	"strconv"
 	"time"
 
+	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
+	"github.com/pbtrad/go-url-shortener/database"
+	"github.com/pbtrad/go-url-shortener/helpers"
 )
 
 type request struct {
@@ -28,6 +33,22 @@ func ShortenURL(c *fiber.Ctx) error {
 	}
 
 	//implementing rate limiter from IP
+	r2 := database.CreateClient(1)
+	defer r2.Close()
+	val, err := r2.Get(databaseCtx, c.IP()).Result()
+	if err == redis.Nil {
+		_ = r2.Set(database.Ctx, c.IP, os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
+	} else {
+		val, _ = r2.Get(database.Ctx, c.IP().Result())
+		valInt, _ := strconv.Atoi(val)
+		if valInt <= 0 {
+			limit, _ := r2.TTL(database.Ctx, c.IP()).Result()
+			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+				"error":            "Rate limit exceeded",
+				"rate_limit_reset": limit / time.Nanosecond / time.Minute,
+			})
+		}
+	}
 
 	//check if input is an actual URL
 
@@ -42,4 +63,6 @@ func ShortenURL(c *fiber.Ctx) error {
 
 	//enforce https, SSL
 	body.URL = helpers.EnforceHTTP(body.URL)
+
+	r2.Decr(database.Ctx, c.IP())
 }
